a9a3d56e4167ab242e60c80289823cd76c9ee4be
Tonywww2 <1204484415@qq.com>
2020-02-01 10:46:46 -0800

    q23rtyu

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        Messenger.AddListener<string>(Events.Interact, AddInventory);
+
+    }
+    private void OnDisable()
+    {
+        Messenger.RemoveListener<string>(Events.Interact, AddInventory);
+
+    }
+
+    void AddInventory(string name)
+    {
+        for(int i = 1; i < 8; i++)
+        {
+            GameObject invTemp = GameObject.Find("Inv" + i);
+            //invTemp.AddComponent<SpriteRenderer>().sprite = name;
+            
+
+        }
+
+    }
+}

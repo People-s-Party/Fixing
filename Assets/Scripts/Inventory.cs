0118b15aa49b5e8889bcab84bc03732c6440d58a
Tonywww2 <1204484415@qq.com>
2020-02-01 13:19:34 -0800

    fdxghjk

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -10,22 +10,38 @@ public class Inventory : MonoBehaviour
     // Start is called before the first frame update
     private void OnEnable()
     {
-        Messenger.AddListener<string>(Events.Interact, AddInventory);
+        Messenger.AddListener<string, Sprite>(Events.itemget, AddInventory);
 
     }
     private void OnDisable()
     {
-        Messenger.RemoveListener<string>(Events.Interact, AddInventory);
+        Messenger.RemoveListener<string, Sprite>(Events.itemget, AddInventory);
 
     }
 
-    void AddInventory(string name)
+    void AddInventory(string name, Sprite sprite)
     {
+        bool isAdded = false;
+        Image tempOut;
         for (int i = 1; i < 8; i++)
         {
             GameObject invTemp = GameObject.Find("Inv" + i);
-            GameObject eItem = GameObject.Find(name);
-            invTemp.AddComponent<Image>().sprite = 
+            if (!invTemp.GetComponent<InventoryInform>().hasItem)
+            {
+                if (!isAdded)
+                {
+                    if (!invTemp.TryGetComponent<Image>(out tempOut))
+                    {
+                        invTemp.AddComponent<Image>().sprite = sprite;
+                        invTemp.GetComponent<InventoryInform>().hasItem = true;
+                        invTemp.GetComponent<InventoryInform>().itemName = name;
+                        isAdded = true;
+
+                    }
+                    
+
+                }
+            }
 
 
         }
---
+++ Assets/Scripts/ItemInform.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemInform : MonoBehaviour
+{
+    public int itemtype;
+    public string needName="";
+
+    private void Start()
+    {
+
+    }
+    private void OnEnable()
+    {
+        Messenger.AddListener<string>(Events.Epressed, action);
+    }
+    private void OnDisable()
+    {
+        Messenger.RemoveListener<string>(Events.Epressed, action);
+    }
+    void action(string name)
+    {
+        if (gameObject.name == name)
+        {
+            if (itemtype == 0)
+            {
+                if (needName == "")
+                {
+                    //Debug.Log(gameObject.name);
+                    //Debug.Log(GetComponent<SpriteRenderer>().sprite);
+                    Messenger.Broadcast<string, Sprite>(Events.itemget, gameObject.name, GetComponent<SpriteRenderer>().sprite);
+                    Destroy(this.gameObject);
+                }
+                else
+                {
+                    for (int i = 1; i < 8; i++)
+                    {
+                        GameObject invTemp = GameObject.Find("Inv" + i);
+                        //Debug.Log(invTemp.GetComponent<InventoryInform>().itemName);
+                        if (needName == invTemp.GetComponent<InventoryInform>().itemName)
+                        {
+                            //Debug.Log(invTemp.GetComponent<InventoryInform>().itemName);
+                            //Debug.Log(GetComponent<SpriteRenderer>().sprite);
+                            Messenger.Broadcast<string, Sprite>(Events.itemget, gameObject.name, GetComponent<SpriteRenderer>().sprite);
+                            Destroy(this.gameObject);
+
+                        }
+
+                    }
+                }
+            }
+            
+        }
+    }
+}

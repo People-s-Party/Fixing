7278b09e404357c412b458c58e400a2f28cf2051
wang3ng <56563071+wang3ng@users.noreply.github.com>
2020-02-01 13:43:11 -0800

    Update ItemInform.cs

---
+++ Assets/Scripts/ItemInform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInform : MonoBehaviour
+{
+    public int itemtype;
+    public string needName="";
+    private void OnEnable()
+    {
+        Messenger.AddListener<string>(Events.Epressed, action);
+    }
+    private void OnDisable()
+    {
+        Messenger.RemoveListener<string>(Events.Epressed, action);
+    }
+    void action(string name)
+    {
+        if (gameObject.name == name)
+        {
+            if (itemtype == 0)
+            {
+                if (needName == "")
+                {
+                    Debug.Log(gameObject.name);
+                    Debug.Log(GetComponent<SpriteRenderer>().sprite);
+                    Messenger.Broadcast<string, Sprite>(Events.itemget, gameObject.name, GetComponent<SpriteRenderer>().sprite);
+                    Destroy(this.gameObject);
+                }
+                else
+                {
+
+                }
+            }
+            
+        }
+    }
+}

687ae8fd5d6b69b776583174729b984a6c550b0e
wang3ng <56563071+wang3ng@users.noreply.github.com>
2020-01-31 21:55:40 -0800

    Gamescenes and some cript

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public Vector3 StartPostion;
+    public float PlayerSpeed;
+    public float rotateSpeed = 2f;
+    void Start()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, StartPostion, 10000);
+    }
+
+    void Update()
+    {
+        Quaternion targetAngels;
+        Vector3 playerposition = transform.position;
+        float MoveX = 0;
+        float MoveY = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveY += Time.deltaTime * PlayerSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            MoveY -= Time.deltaTime * PlayerSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveX -= Time.deltaTime * PlayerSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveX += Time.deltaTime * PlayerSpeed;
+        }
+        if (playerposition.x + MoveX <= -8 || playerposition.x + MoveX >= 8.5) MoveX = 0;
+        if (playerposition.y + MoveY <= -4.5 || playerposition.y + MoveY >= 3) MoveY = 0;
+        transform.Translate(MoveX, MoveY, 0);
+    }
+}

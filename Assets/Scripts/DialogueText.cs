fe215c1f73676b1e9899ba75738f07707a85605e
Tonywww2 <1204484415@qq.com>
2020-02-01 09:58:12 -0800

    123

---
+++ Assets/Scripts/DialogueText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueText : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Messenger.AddListener<string>(Events.Interact, PrintText);
+
+    }
+    private void OnDisable()
+    {
+        Messenger.RemoveListener<string>(Events.Interact, PrintText);
+
+    }
+
+    void PrintText(string name)
+    {
+        GetComponentInChildren<TextMesh>().text = name;
+
+    }
+}

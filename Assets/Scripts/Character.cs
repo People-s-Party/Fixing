20af3e93803c0d13a5242d46cec1a31f70c123cd
wang3ng <56563071+wang3ng@users.noreply.github.com>
2020-01-31 22:14:43 -0800

    Moving

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -20,10 +20,12 @@ public class Character : MonoBehaviour
         float MoveY = 0;
         if (Input.GetKey(KeyCode.W))
         {
+            targetAngels = Quaternion.Euler(0, 0, 0);
             MoveY += Time.deltaTime * PlayerSpeed;
         }
         if (Input.GetKey(KeyCode.S))
         {
+            targetAngels = Quaternion.Euler(0, 180f, 0);
             MoveY -= Time.deltaTime * PlayerSpeed;
         }
         if (Input.GetKey(KeyCode.A))
@@ -36,6 +38,13 @@ public class Character : MonoBehaviour
         }
         if (playerposition.x + MoveX <= -8 || playerposition.x + MoveX >= 8.5) MoveX = 0;
         if (playerposition.y + MoveY <= -4.5 || playerposition.y + MoveY >= 3) MoveY = 0;
-        transform.Translate(MoveX, MoveY, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetAngels, rotateSpeed * Time.deltaTime);
+        if (Quaternion.Angle(targetAngels, transform.rotation) < 1)
+        {
+            transform.rotation = targetAngels;
+        }
+        if (MoveX!=0)transform.Translate(MoveX, 0, 0);
+        else transform.Translate(0, MoveY, 0);
+
     }
 }
